542a08323771b2ae5a687bc82bdc0cbd180d9838
Arne Diekmann <arne@neoskop.de>
2023-11-27 13:16:23 +0100

    feat: add etag support

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import fastify from "fastify";
 import { resizeRequestHandler } from "./resize/resize-request-handler.js";
 import { logger } from "./util/logger.js";
+import etag from "@fastify/etag";
 
 const app = fastify({
   logger: process.env.DEBUG === "true" ? logger : false,
 });
 
+app.register(etag);
 app.get("/*", resizeRequestHandler);
-
 app.listen({ port: 8080, host: "0.0.0.0" }).catch((err) => {
   console.error(err);
   process.exit(1);
