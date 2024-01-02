f7796d4bfb8e05e60d7cdfafcd47e49db093a41f
Arne Diekmann <arne@neoskop.de>
2024-01-02 16:30:59 +0100

    chore: prepare release

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 increment=${1:-patch}
-version=$(npm version ${increment})
-docker build -t neoskop/ixy:${version} .
-docker push neoskop/ixy:${version}
+
+build() {
+    cd $1
+    version=$(npm version ${increment} --no-git-tag-version)
+    docker build -t neoskop/${1}:${version} .
+    docker push neoskop/${1}:${version}
+    cd - &>/dev/null
+}
+
+git add .
+git commit -m "chore: release ${version}"
+git push
