e7f4c4402726f31e5abad0b28a8202408fcf4b3d
Arne Diekmann <arne@neoskop.de>
2024-02-29 15:09:19 +0100

    feat: add helm release

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,8 +11,24 @@ increment_version() {
 increment_version frontend ixy-ui
 increment_version backend ixy
 msg="chore: release ${version}"
+sed -i "s/appVersion: .*/appVersion: \"$version\"/" helm/Chart.yaml
+sed -i "s/version: .*/version: $version/" helm/Chart.yaml
+yq eval ".version=\"$version\"" -i helm/Chart.yaml
+yq eval ".appVersion=\"$version\"" -i helm/Chart.yaml
+yq eval ".image.tag=\"$version\"" -i helm/values.yaml
+yq eval ".ui.image.tag=\"$version\"" -i helm/values.yaml
 git add .
 git commit -m "$msg"
 git tag -a $version -m "$msg"
 git push origin $version
 git push
+
+helm package helm --destination .deploy
+cr upload -o neoskop -r ixy -p .deploy
+git checkout gh-pages
+cr index -i ./index.yaml -p .deploy -o neoskop -r ixy -c https://neoskop.github.io/ixy/
+git add index.yaml
+git commit -m "chore: Bump version to ${version}."
+git push
+git checkout master
+rm -rf .deploy/
